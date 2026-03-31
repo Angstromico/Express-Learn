330857a412c530868726876201ac3ef0f65772d5
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-31 05:32:35 -0400

    feat: mountains avif

--- src/services/publicFiles/get-mountains.ts
+++ src/services/publicFiles/get-mountains.ts
@@ -1,7 +1,7 @@
 import patch from 'path'
 
 function getMountains() {
-  const mountains = patch.join(process.cwd(), 'public', 'mountains')
+  const mountains = patch.join(process.cwd(), 'public', 'mountains.avif')
   return mountains
 }
 
